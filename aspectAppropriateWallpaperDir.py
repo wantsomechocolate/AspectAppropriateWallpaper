3e9e7e95feab84548b1309cc7b9b7186ac369da7
James McGlynn <wantsomechocolate@gmail.com>
2012-12-19 17:48:55 -0500

    Almost Ready to Start Debugging

--- aspectAppropriateWallpaperDir.py
+++ aspectAppropriateWallpaperDir.py
@@ -28,48 +28,52 @@ tol=0.15
 screen_dim1=GetSystemMetrics (0)
 screen_dim2=GetSystemMetrics (1)
 screen_change=(screen_dim1+screen_dim2)/2
+screen_aspect_ratio=float(screen_dim1)/float(screen_dim2)
+
+print screen_aspect_ratio
 
 for item in dirContents:
     item_path=directory+"\\"+item
+    #print item_path
+    print item
     if os.path.isfile(item_path):
         if imgExtCheck(item_path):
             im = Image.open(item_path)
             asRat = float(im.size[0])/float(im.size[1])
-            if asRat>=(1-tol)*
-            landscapeList.append(landscapeDir+"\\"+item)
-    else:pass
+            print asRat
+            print 1/asRat
+            if asRat>=(1-tol)*screen_aspect_ratio and asRat<=(1+tol)*screen_aspect_ratio:
+                landscapeList.append(directory+"\\"+item)
+            elif asRat>=(1-tol)*(1/screen_aspect_ratio) and asRat<=(1+tol)*(1/screen_aspect_ratio):
+                portraitList.append(directory+"\\"+item)
 
-for item in portraitDirContents:
-    if os.path.isfile(portraitDir+"\\"+item):
-        portraitList.append(portraitDir+"\\"+item)
-    else:pass
+##Holy D!##$ It Works!   
+print landscapeList
+print portraitList
 
-
-##print landscapeList
-##print portraitList
-
-lc=0
-pc=0
-lll=len(landscapeList)
-pll=len(portraitList)
-cli=landscapeList[lc]
-cpi=portraitList[pc]
+##So now I can continue as before with two separate lists one containing the "landscape images"
+##and one containing the portrait ones. 
 
 
-
+##lc=0
+##pc=0
+##lll=len(landscapeList)
+##pll=len(portraitList)
+##cli=landscapeList[lc]
+##cpi=portraitList[pc]
 
 #print initialWidth, lll
 
-if initialWidth>screenChange:
-    currentOrientation="Landscape"
-    lc=int((random.random()*lll))
-    ctypes.windll.user32.SystemParametersInfoA(SPI_SETDESKWALLPAPER, 0, landscapeList[lc] , 0)
-else:
-    currentOrientation="Portrait"
-    pc=int((random.random()*pll))
-    ctypes.windll.user32.SystemParametersInfoA(SPI_SETDESKWALLPAPER, 0, portraitList[pc] , 0)
-
-print currentOrientation,lc,landscapeList[lc]
+##if initialWidth>screenChange:
+##    currentOrientation="Landscape"
+##    lc=int((random.random()*lll))
+##    ctypes.windll.user32.SystemParametersInfoA(SPI_SETDESKWALLPAPER, 0, landscapeList[lc] , 0)
+##else:
+##    currentOrientation="Portrait"
+##    pc=int((random.random()*pll))
+##    ctypes.windll.user32.SystemParametersInfoA(SPI_SETDESKWALLPAPER, 0, portraitList[pc] , 0)
+##
+##print currentOrientation,lc,landscapeList[lc]
 
 
 ##true = 1
