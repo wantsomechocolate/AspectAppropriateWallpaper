6ce0d8cbd99f40003c81bd66dc6791ad432bbdbf
James McGlynn <wantsomechocolate@gmail.com>
2012-12-20 17:45:51 -0500

    Add setup.py
    Adding this file to make it easier for other people to use

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+import py2exe
+
+setup(windows=['aspectAppropriateWallpaperDir.py'])
+
+## Type python setup.py py2exe
+## And put a shorcut to the exe in
+## C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup
