919b09cb1cdb147a2abec04d1ce769c1953f0599
James McGlynn <wantsomechocolate@gmail.com>
2012-12-22 15:32:22 -0500

    Almost There
    Added extension checking and some more comments.
    Changed the name of the file to aawp.py
    and updated the setup file

--- setup.py
+++ setup.py
@@ -1,8 +1,11 @@
 from distutils.core import setup
 import py2exe
 
-setup(windows=['aspectAppropriateWallpaperDir.py'])
+setup(windows=['aawp.py'])
 
+## Download and install py2exe
+## Navigate to the directory with the setup file and aawp.py
 ## Type python setup.py py2exe
 ## And put a shorcut to the exe in
 ## C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup
+## Or wherever your startup folder is. 
